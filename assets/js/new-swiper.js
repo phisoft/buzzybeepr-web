6ef0369890fcf3df0d641ebf40ad162d7aaadb27
norizwana <ezwana19@gmail.com>
2023-03-22 10:55:03 +0800

    adjust padding-height #10

--- assets/js/new-swiper.js
+++ assets/js/new-swiper.js
@@ -6,17 +6,17 @@
     speed: 600,
     loop: true,                         //loop
     autoplay: {
-        delay: 5000,
+        delay: 3000,
         disableOnInteraction: false
       },
     slidesPerView: 3,      
     spaceBetween: 30,              //how many slides to show
     centeredSlides: true,               //centering the first slide
-    pagination: {                       //pagination(dots)
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
+    // pagination: {                       //pagination(dots)
+    //     el: '.swiper-pagination',
+    //     type: 'bullets',
+    //     clickable: true
+    // },
     // navigation: {                       
     //     nextEl: ".swiper-button-next",
     //     prevEl: ".swiper-button-prev",
